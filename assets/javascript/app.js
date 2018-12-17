b014c297a3a6b3b8ee5df2474cf101c0d4a266fb
Kaloli <rvellark@gmail.com>
2018-12-16 20:25:11 -0700

    updatedec15_2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,54 @@
 $(document).ready(function () {
-
-var winner = 0;
-var count = 100;
-
-
-var counter=setInterval(timer, 1000);
-var data= "0"
-
-function timer()
-{
-
-  count=count-1;
-  $("#countDown").html('Time left: ' + count)
-  if (count <= 0)
-  
-  {
-     clearInterval(counter);
-     //counter ended, do something here
-     return;
-  }
-  
-
-  //Do code for showing the number of seconds here
-}
-
-$( "#GreatSmoky" ).on( "click", function() {
-  winner++ ;
-  console.log(winner);
-});
-
-$( "#LakeMead" ).on( "click", function() {
-  winner++ ;
-  console.log(winner);
-});
-$( "#wrangell" ).on( "click", function() {
-  winner++ ;
-  console.log(winner);
-});
-$( "#YellowstoneFour" ).on( "click", function() {
-  winner++ ;
-  console.log(winner);
-});
-$( "#button" ).on( "click", function() {
-  alert(winner);
-});
+  $("#formButton").click(function () {
+    $("#npQuiz").show();
 
 
 
+    var winner = 0;
+    var count = 5;
+
+
+
+    var counter = setInterval(timer, 1000);
+
+
+    function timer() {
+
+      count = count - 1;
+      $("#countDown").html('Time left: ' + count)
+      if (count === 0)
+
+      {
+        clearInterval(counter);
+        $("#countDown").html('Time has run out ')
+      }
+
+
+      //Do code for showing the number of seconds here
+    }
+
+    $("#GreatSmoky").one("click", function () {
+      winner++;
+      console.log(winner);
+    });
+
+    $("#LakeMead").one("click", function () {
+      winner++;
+      console.log(winner);
+    });
+    $("#wrangell").on("click", function () {
+      winner++;
+      console.log(winner);
+    });
+    $("#YellowstoneFour").on("click", function () {
+      winner++;
+      console.log(winner);
+    });
+    $("#button").on("click", function () {
+      alert(winner);
+    });
+
+
+  });
 
 });
